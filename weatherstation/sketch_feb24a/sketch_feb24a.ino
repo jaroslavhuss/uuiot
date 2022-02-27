8084ce450ff268c1b58a082b26d1dc7157c4e791
Jaroslav <jaroslav.huss@plus4u.net>
2022-02-27 10:23:10 +0100

    fixed code for weather station and latest node-red setup exported

---
+++ weatherstation/sketch_feb24a/sketch_feb24a.ino
@@ -0,0 +1,34 @@
+#include "DHT.h"
+#define DHTPIN 2
+#define DHTTYPE DHT22
+DHT dht(DHTPIN, DHTTYPE);
+// Vars for Humidity and temperatrue
+float hum;
+float temp;
+
+// Get rid of fakin delay
+unsigned long previousMillis = 0;
+const long interval = 2000;
+
+void setup()
+{
+  Serial.begin(9600);
+  dht.begin();
+}
+
+void loop()
+{
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= interval)
+  {
+    previousMillis = currentMillis;
+    hum = dht.readHumidity();
+    temp = dht.readTemperature();
+
+    Serial.print("Vlhkost: ");
+    Serial.print(hum);
+    Serial.print(" %, Teplota: ");
+    Serial.print(temp);
+    Serial.println(" Celsius");
+  }
+}
