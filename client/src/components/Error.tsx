2fdaa6d61d963522e0c07676f3d10d1da3a8cb53
JAN VOJACEK <vojacekjan@hotmail.com>
2022-05-30 12:19:25 +0200

    Display errors from manage pages properly through error reducer

--- client/src/components/Error.tsx
+++ client/src/components/Error.tsx
@@ -1,29 +1,39 @@
 import { useDispatch } from "react-redux";
-import { cleanError } from "../store/reducers/errorReducer"
-const Error = ({ errorMessage }: { errorMessage: [] }) => {
-    const dispatch = useDispatch();
-    console.log(errorMessage)
-    return (
-        <div style={{
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            position: "fixed",
-            zIndex: 99999,
-            width: "100%",
-            height: "100%",
-            background: "#e5e5e5d1"
-        }}
-            onClick={() => {
-                dispatch(cleanError());
-            }}
-        >
-            {
-                //   errorMessage.map((err, index) => <p key={index} style={{ color: "red", padding: 40, background: "white", borderRadius: 12 }}>{err}</p>)
-            }
+import { cleanError, ErrorStateAlert } from "../store/reducers/errorReducer";
 
-        </div>
-    );
+const Error = ({ errorMessage }: { errorMessage: Array<ErrorStateAlert> }) => {
+  const dispatch = useDispatch();
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        position: "fixed",
+        zIndex: 99999,
+        width: "100%",
+        height: "100%",
+        background: "#e5e5e5d1",
+      }}
+      onClick={() => {
+        dispatch(cleanError());
+      }}
+    >
+      {errorMessage.map((err, index) => (
+        <p
+          key={index}
+          style={{
+            color: "red",
+            padding: 40,
+            background: "white",
+            borderRadius: 12,
+          }}
+        >
+          {err.message}
+        </p>
+      ))}
+    </div>
+  );
 };
 
 export default Error;
